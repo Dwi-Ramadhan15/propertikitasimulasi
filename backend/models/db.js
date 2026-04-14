229ccaeb9d9ca7b4fcc48d57b0904d87d8ba5a68
Dwi_Ramadhan <dwir57017@gmail.com>
2026-04-14 11:43:00 +0700

    fix vercel json

--- backend/models/db.js
+++ backend/models/db.js
@@ -6,10 +6,8 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 5432, // Port default PostgreSQL
+    port: 5432,
 });
-
-// Test koneksi saat file ini dipanggil
 pool.connect((err, client, release) => {
     if (err) {
         return console.error('Error saat koneksi ke PostgreSQL:', err.stack);
